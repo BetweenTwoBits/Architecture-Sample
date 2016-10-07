e64126e1f5df6f7030376fdcc9a9b715c6f291b2
Brandon Davis <brandon.davis@possible.com>
2016-10-07 16:21:39 -0600

    convert controller callback to kotlin

--- app/src/main/java/com/possible/architecturesample/data/network/ControllerCallback.java
+++
@@ -1,9 +0,0 @@
-package com.possible.architecturesample.data.network;
-
-public class ControllerCallback<T> {
-    public void onControllerNext(T t) {}
-
-    public void onControllerComplete() {}
-
-    public void onControllerError(Exception exception) {}
-}
---
+++ app/src/main/java/com/possible/architecturesample/data/network/ControllerCallback.kt
@@ -0,0 +1,9 @@
+package com.possible.architecturesample.data.network
+
+class ControllerCallback<in T> {
+    fun onControllerNext(t: T) {}
+
+    fun onControllerComplete() {}
+
+    fun onControllerError(exception: Exception) {}
+}
